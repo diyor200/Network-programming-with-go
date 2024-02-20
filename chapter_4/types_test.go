e56629ce92dd2b1bfed9aaad654bddf5d3387f3c
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2024-02-20 21:17:38 +0500

    Test MaxPayload size

--- chapter_4/types_test.go
+++ chapter_4/types_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"net"
 	"reflect"
 	"testing"
@@ -51,6 +53,26 @@ func TestPayloads(t *testing.T) {
 			continue
 		}
 		t.Logf("[%T] %[1]q", actual)
+	}
+
+}
+
+func TestMaxPayloadSize(t *testing.T) {
+	buf := new(bytes.Buffer)
+	err := buf.WriteByte(BinaryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = binary.Write(buf, binary.BigEndian, uint32(1<<30)) // 1 GB
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b Binary
+	_, err = b.ReadFrom(buf)
+	if err != ErrMaxPayloadSize {
+		t.Fatalf("expected ErrMaxPayloadSize error; actual %v", err)
 	}
 
 }
